fc86ac60c37eb2feeb97aceec9ab7523058cf4f5
GoliSkyWalker <golihn@gmail.com>
2018-04-14 10:45:01 +0430

    instead of add Hotel class

---
+++ AdminHoteManagementClass.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Security.Cryptography;
+
+
+namespace HotelDataBase
+{
+
+    public  class AdminHotelListManipulationClass
+    {
+        HotelDataBase.TableTypes.Hotel currentHotel;
+        public const string CONNECTION_STRING = "Data Source=DESKTOP-SSOCU79;"
+            +"Initial Catalog = HotelModel ;"
+            +" Integrated Security = True";
+        static TableTypes.Hotel hotelInstance;
+        public string addHotel(TableTypes.Hotel hotel)
+        {
+            string log ="";
+            //add Hotel to Hotel table
+            AdminHotelListManipulationClass.hotelInstance = hotel;
+            log+=AddHotelToSqlTables(hotel);
+            return log;
+        }
+       
+        private string AddHotelToSqlTables(HotelDataBase.TableTypes.Hotel hotel)
+        {
+            //create query and run it
+            runQuery(constructQuery(hotel));
+            return "";
+        }
+        
+        
+       
+
+        private String AddEmployeesToHotel(UserTypes.Employee emp)
+        {
+            
+            if (hotelInstance == null)
+            {
+                return "hotel instance not initiated";
+            }
+            else
+            {
+               return runQuery(constructQuery(emp)); 
+            }
+            
+        }
+        private String AddRoomsToHotel(TableTypes.Room room)
+        {
+            
+            if (hotelInstance == null)
+            {
+                return "hotel instance not initiated";
+            }
+            else
+            {
+                return runQuery(constructQuery(room)); 
+            }
+            
+        }
+
+        public string UpdateHotelPrices(String roomType, float newPrice)
+        {
+            string query = "update room_type"
+                           + "set room_price = " + newPrice
+                           + "Where room_Type = " + roomType;
+            return runQuery(query);
+
+        }
+        
+        
+        private string constructQuery(object queryObject)
+        {
+            if (queryObject is TableTypes.Hotel)
+            {
+                TableTypes.Hotel hotel = (TableTypes.Hotel)queryObject;
+                return "insert into Hotel(Hotel_Id, HotelName, Addrs, City, Phone_no)" +
+                    "values('" + hotelInstance.Hotel_Id + "', '" + hotelInstance.Name + "', '"
+                    + hotelInstance.Addr + "', '" + hotelInstance.Addr + "'," + hotelInstance.Phone_Num + ");";
+            }
+            
+            if(queryObject is UserTypes.Employee)
+            {
+                UserTypes.Employee employee = (UserTypes.Employee)queryObject;
+                return "insert into Employee( First_name , Last_name , DOB , Addrs , Phone_no , Email , Pass , Hotel_Id )" +
+            "values('" + employee.First_Name + "', '" + employee.Last_name + "', '" + employee.Date_Of_Birth + "', '" + employee.Addr + "', '" + employee.Phone_Num + "', '" + employee.Email + "', '" +employee.Pass + "'," + hotelInstance.Hotel_Id + ")";
+            }
+            if (queryObject is TableTypes.Room)
+            {
+                TableTypes.Room room = (TableTypes.Room) queryObject;
+                return "insert into Room (Room_Id  ,Hotel_ID ,Room_Type ,Room_Description)" 
+                    +"values(" + room.Room_Id + "," + hotelInstance.Hotel_Id + ", '" + room.Room_Type + "', '" + room.Room_Description + "')";
+            }
+            return "";
+
+
+
+        }
+        
+        private string runQuery(string query)
+        {
+            SqlConnection conn = new SqlConnection();
+
+            conn.ConnectionString = CONNECTION_STRING;
+
+            try
+
+            {
+                conn.Open();
+                SqlCommand command = new SqlCommand(query, conn);
+                command.ExecuteNonQuery();
+                conn.Close();
+                return "Query "+ query + "was successfully executed";
+            }
+
+            catch (Exception ex)
+
+            {
+
+                Console.WriteLine("Can not open connection ! " + ex.Message);
+                return "sql request error , hotel management failed ";
+
+            }            
+        }
+
+    }
+
+    
+}
---
+++ CustomerLogin.cs
@@ -0,0 +1,118 @@
+﻿using System.Data;
+using HotelDataBase;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.IO;
+using System.Web;
+
+namespace HotelDataBaseProj
+{
+    // a class to create info for customrer to login
+    public class CustomerLogin
+    {
+        public const string CONNECTION_STRING = "Data Source=LOCALHOST;" +
+                                                "Initial Catalog=HotelModel;" +
+                                                "User id=SA;" +
+                                                "Password=f3l!Xkjil;";
+
+        public string CreateAccount(UserTypes.Customer newCustomer)
+        {
+            string log = "";
+
+
+            if (checkForDuplicate(newCustomer))
+                log += EnterIntoCustomerTable(newCustomer);
+            else
+            {
+                return "customer already exists";
+            }
+
+            return "customer " + newCustomer.First_Name + "has an account now.";
+
+
+        }
+
+        private string EnterIntoCustomerTable(UserTypes.Customer newCustomer)
+        {
+            string query = "insert into customers (First_name, Last_name, Addrs, Phone_no, Email,Pass)" +
+                           "values  ('" + newCustomer.First_Name + "', '" + newCustomer.Last_name+ "' , '" + newCustomer.Addr + "'" +
+                           " ," + newCustomer.Phone_Num + ",'" +newCustomer.Email+ "','" +newCustomer.Pass+ "' );";
+            return runInsertNewSignUpQuery(query);
+        }
+        private bool checkForDuplicate(UserTypes.Customer newCustomer)
+        {
+            string qurey = "Select * From Customer where email = '" + newCustomer.Email + "";
+            return runCheckDuplicateQuery(qurey);
+
+
+        }
+        private string runInsertNewSignUpQuery(string query)
+        {
+            string log = "";
+            SqlConnection conn = new SqlConnection();
+
+            conn.ConnectionString = CONNECTION_STRING;
+            try
+            {
+                conn.Open();
+                SqlCommand command = new SqlCommand(query, conn);
+                command.ExecuteNonQuery();
+                command.Dispose();
+                SqlDataReader reader = command.ExecuteReader();
+                log += "insert new sigh up customer query executed";
+                conn.Close();
+            }
+
+            catch (Exception ex)
+
+            {
+                Console.WriteLine("Can not open connection ! " + ex.Message);
+                log += "opetration add new customer failed with error" + ex.Message;
+            }
+
+            return log;
+        }
+
+       
+
+        private bool runCheckDuplicateQuery(string query)
+        {
+            SqlConnection conn = new SqlConnection();
+            string log ="";
+            conn.ConnectionString = CONNECTION_STRING;
+            try
+            {
+                conn.Open();
+                SqlCommand command = new SqlCommand(query, conn);
+                command.ExecuteNonQuery();
+                command.Dispose();
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    if (!reader.HasRows)
+                    {
+                        return true;
+                    }
+                    else
+                        return false;
+
+
+                }
+                log += "command executed";
+                conn.Close();
+            }
+
+            catch (Exception ex)
+
+            {
+                Console.WriteLine("Can not open connection ! " + ex.Message);
+                return false;
+            }
+
+            return false;
+
+        }
+    }
+}
